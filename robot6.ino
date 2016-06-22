a94ee6a9377f946dae1a229ff2c429b57cc1f647
nstreeter <nstreeter@cca.edu>
2016-06-22 14:13:36 -0700

    Add files via upload

--- robot6.ino
+++ robot6.ino
@@ -1,60 +1,65 @@
 //basics for left side of the bot
-const int lsc = 6; // PWM pin for speed control
-const int lw1 = 2;
-const int lw2 = 4;
+const int lsc = 9; // PWM pin for speed control
+const int lw1 = 7;
+const int lw2 = 8;
 
 //basics for right side of bot
-const int rsc = 9;
-const int rw1 = 7;
-const int rw2 = 8;
+const int rsc = 4;
+const int rw1 = 2;
+const int rw2 = 6;
 
 //light sensors
 const int lldr = A0;
 const int rldr = A1;
 
 //ultrasonic
-const int trig = 10;
-const int echo = 11;
+const int trig = 11;
+const int echo = 12;
 
+//turns robot left
 void robotLeft() {
-  Serial.print("robot_left");
+  Serial.println("robot_left");
   // left motor reverse
   digitalWrite(lw1, LOW);
   digitalWrite(lw2, HIGH);
   // right motor forwards
-  digitalWrite(rw1, HIGH);
-  digitalWrite(rw2, LOW);
-}
-
-void robotRight() {
-  Serial.print("robot_right");
-  //left motor forwards
-  digitalWrite(lw1, HIGH);
-  digitalWrite(lw2, LOW);
-  //right motor reverse
   digitalWrite(rw1, LOW);
   digitalWrite(rw2, HIGH);
 }
 
+//turn robot right
+void robotRight() {
+  Serial.println("robot_right");
+  //left motor forwards
+  digitalWrite(lw1, HIGH);
+  digitalWrite(lw2, LOW);
+  //right motor reverse
+  digitalWrite(rw1, HIGH);
+  digitalWrite(rw2, LOW);
+}
+
+//move robot forwards
 void robotForwards() {
-  Serial.print("robot_forwards");
+  Serial.println("robot_forwards");
   //left motor forwards
   digitalWrite(lw1, HIGH);
   digitalWrite(lw2, LOW);
   //right motor forwards
-  digitalWrite(rw1, HIGH);
-  digitalWrite(rw2, LOW);
+  digitalWrite(rw1, LOW);
+  digitalWrite(rw2, HIGH);
 }
 
+//move robot backwards
 void robotBackwards() {
-  Serial.print("robot_backwards");
+  Serial.println("robot_backwards");
   //left motor reverse
   digitalWrite(lw1, LOW);
   digitalWrite(lw2, HIGH);
   //right motor reverse
-  digitalWrite(rw1, LOW);
-  digitalWrite(rw2, HIGH);
+  digitalWrite(rw1, HIGH);
+  digitalWrite(rw2, LOW);
 }
+
 
 
 void setup () {
@@ -68,6 +73,10 @@ void setup () {
   pinMode(rw1, OUTPUT);
   pinMode(rw2, OUTPUT);
 
+  //activate ultrasonic sensors
+  pinMode(trig, OUTPUT);
+  pinMode(echo, INPUT);
+
   // activate photocells
   analogWrite (lsc, 200);
   analogWrite (rsc, 200);
@@ -78,36 +87,49 @@ void setup () {
 void loop () {
 
   long duration, distance;
+
+  //sends pulse
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
+
+  //measure time between echos, tells how far away objects are
   duration = pulseIn(echo, HIGH);
+
+  //calculates distance to object in cm
   distance = (duration / 2) / 29.1;
+  Serial.println(distance);
+  Serial.print("cm");
 
+  //actions if distance>10
   if ( distance >= 10) {
-    analogRead(lldr);
-    analogRead(rldr);
-  }
-  else if (analogRead(lldr) > analogRead(rldr)) {
-    robotLeft();
-    delay(150);
     robotForwards();
-    delay(1000);
-  }
-  else if (analogRead(lldr) < analogRead(rldr)) {
-    robotRight();
     delay(500);
-    robotForwards();
-    delay(1000);
-  }
-  else {
+
+    if (analogRead(lldr) > analogRead(rldr)) {
+      Serial.println("left");
+      robotLeft();
+      delay(500);
+      robotForwards();
+      delay(2000);
+    }
+    else if (analogRead(lldr) < analogRead(rldr)) {
+      Serial.println("right");
+      robotRight();
+      delay(500);
+      robotForwards();
+      delay(2000);
+    }
+  } // end of distnace > 10
+
+  //actions if distance<10
+  if ( distance <= 10) {
+    Serial.println("reverse");
     robotBackwards();
-    delay(500);
-
+    delay(3000);
     robotRight();
-
     delay(500);
   }
 }
